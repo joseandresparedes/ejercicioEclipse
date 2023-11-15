10ca765c7fedcfa33ea26764e0e577e18b3841c8
Jose Andres Paredes Arribas <joseandres.paredes@viewnext.com>
2023-11-15 16:02:11 +0100

    Se ha creado la clase syntax error

---
+++ ejercicioEclipse/src/ejercicioEclipse/SyntaxError.java
@@ -0,0 +1,16 @@
+package ejercicioEclipse;
+
+import java.util.*;
+
+public class SyntaxError {
+    public static void printString() {
+	    String str = "Hola";
+	    Random rand = new Random();
+	    for (int i = 0; i < 3; i++){
+	        System.out.println(str);		
+	    }
+    }
+    public static void main(String args[]) {
+        printString();
+    }
+}
