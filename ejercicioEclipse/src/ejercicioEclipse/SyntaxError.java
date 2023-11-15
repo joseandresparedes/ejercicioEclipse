d232d7538923ed872d6c794376a81f742b498491
joseandresparedes <150909819+joseandresparedes@users.noreply.github.com>
2023-11-15 16:17:31 +0100

    Apartado 7

--- ejercicioEclipse/src/ejercicioEclipse/SyntaxError.java
+++ ejercicioEclipse/src/ejercicioEclipse/SyntaxError.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class SyntaxError {
     public static void printString() {
-	    String str = "Hola, que tal?";
+	    String str = "Me has escuchado?";
 	    Random rand = new Random();
 	    for (int i = 0; i < 3; i++){
 	        System.out.println(str);		
